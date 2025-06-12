ffa1bb1e8306b9a2e0235d266740eddd457dacca
Axmadjon2101 <Ahmadjon.210@icloud.com>
2025-06-12 16:29:39 +0500

    Initial commit

---
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF420942)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFD92157)
+
+val Purple40 = Color(0xFF08060A)
+val PurpleGrey40 = Color(0xFF5009E8)
+val Pink40 = Color(0xFF1A090F)
